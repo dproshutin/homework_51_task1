bd93c9701d04f1f82fccd88b45d23d52905a0edd
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-09-26 18:50:46 +0600

    Class container is added.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Movie from "./Movie";
 function App() {
   return (
     <div className="App">
-      <Movie image="img/stylists.jpg" name="Стиляги" date="2008" />
-      <Movie image="img/sitter.jpg" name="Няня" date="2018" />
-      <Movie image="img/return_ticket.jpg" name="Обратный билет" date="2012" />
+      <div className="container">
+        <Movie image="img/stylists.jpg" name="Стиляги" date="2008" />
+        <Movie image="img/sitter.jpg" name="Няня" date="2018" />
+        <Movie image="img/return_ticket.jpg" name="Обратный билет" date="2012" />
+      </div>
     </div>
   );
 }
